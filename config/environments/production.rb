20ea0c44d96ce52590c60b96699db813c6758999
pooja <pooja.apsolute@gmail.com>
2014-11-10 16:26:07 +0530

    change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.default_url_options = {:host => 'herokuapp:3000'}
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.mandrillapp.com",
     :port           => 587,
+    :domain         =>'heroku.com',
     :authentication => :plain,
     :user_name      => "pooja.apsolute@gmail.com",
     :password       => Rails.application.secrets.mandrill_api_key
